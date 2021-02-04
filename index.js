7694e860df543e9eb85b84fb8100aa353f188ab4
Zoldykk <zakariaatattou4@outlook.com>
2021-02-04 20:16:32 +0100

    version 1.0

--- index.js
+++ index.js
@@ -25,9 +25,6 @@ app.use('/', routes)
 
 if(process.env.NODE_ === 'production'){
   app.use(express.static('client/build'))
-  app.get('*', (req, res) =>{
-    res.render(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
 }
 
 app.listen(port, () => {
