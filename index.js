b90e5888c456a2cce44e37ed3bc76571b31f7e87
Zoldykk <zakariaatattou4@outlook.com>
2021-02-05 10:05:28 +0100

    version 1.0

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use('/images',express.static('uploads'))
 
 app.use('/', routes)
 
-if(process.env.NODE_ === 'production'){
+if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'))
   app.get('*', (req, res) =>{
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
