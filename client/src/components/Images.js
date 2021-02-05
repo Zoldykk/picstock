f051b855c8ca047e2e853c4660a2f0c384d1d41c
Zoldykk <zakariaatattou4@outlook.com>
2021-02-05 10:11:01 +0100

    version 1.0

--- client/src/components/Images.js
+++ client/src/components/Images.js
@@ -26,7 +26,7 @@ export default function Images(props) {
                     { props.loading ? <p>Loading...</p> : <> 
                         {selectedImages.map(image =>(
                             <div key={image._id} className="img-item">
-                                <a className='img' href={`/images/${image._id}`}><img  alt='img' src={`http://localhost:3000/images/${image.imageName}`}/></a>
+                                <a className='img' href={`/images/${image._id}`}><img  alt='img' src={`/images/${image.imageName}`}/></a>
                                 <div className="options">
                                     <i onClick={saveToLocalStorage} dataid={image.imageName} className="fas fa-heart like-btn"></i>
                                 </div>
