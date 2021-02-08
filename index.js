46e8d861ff125b7950b8c37b061369e932231590
Zoldykk <zakariaatattou4@outlook.com>
2021-02-08 16:55:28 +0100

    make it better

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 const port = process.env.PORT || 5000;
 
 // DB Config
-const URI = process.env.dbURI;
+const URI = process.env.dbURI || 'mongodb+srv://zaki:zaki@cluster0.08v1z.mongodb.net/Cluster0?retryWrites=true&w=majority';
 mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true },  () => {console.log('Connected to Db')})
 
 //Middlewares
